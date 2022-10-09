ec262364931cdea62348b47bae7a0185bad199ae
alan <alan@groot.icu>
2022-10-09 22:22:45 +0800

    微信早安天气预报推送, 第二板

--- src/main/java/icu/groot/test/GitTest.java
+++ src/main/java/icu/groot/test/GitTest.java
@@ -11,5 +11,6 @@ package icu.groot.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git第二版");
     }
 }
